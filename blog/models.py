9f9f10d98d906c719136c1a95f4bfc624161d706
afshinshiri <afshinshiri0211@gmail.com>
2021-05-18 18:26:26 +0430

    paginator,include

--- blog/models.py
+++ blog/models.py
@@ -31,8 +31,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:post_de'
-                       'tail',
+        return reverse('blog:post_detail',
                        args=[self.publish.year,
                              self.publish.month,
                              self.publish.day,
