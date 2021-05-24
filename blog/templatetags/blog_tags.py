30da7ff159238e6f0642427a1c4852352dced00a
afshinshiri <afshinshiri0211@gmail.com>
2021-05-24 11:29:42 +0430

    load blog_tags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,26 @@
+from ..models import Post
+from django import template
+from django.db.models import Count
+
+register = template.Library()
+
+
+
+
+
+@register.simple_tag
+def total_post():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_post.html')
+def show_latest_post(count=3):
+    latest_post = Post.published.order_by('-publish')[:count]
+    return {'latest_post': latest_post}
+
+
+@register.inclusion_tag('blog/post/commented_post.html')
+def get_most_commented_posts(count=3):
+    commented = Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+    return {'commented': commented}
+
