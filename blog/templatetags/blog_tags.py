77067bbfdfb9e6829d0016f40d0369d83f6378bb
afshinshiri <afshinshiri0211@gmail.com>
2021-05-24 12:41:39 +0430

    custom filter plus

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,11 +1,10 @@
 from ..models import Post
 from django import template
 from django.db.models import Count
+import markdown
+from django.utils.safestring import mark_safe
 
 register = template.Library()
-
-
-
 
 
 @register.simple_tag
@@ -23,4 +22,9 @@ def show_latest_post(count=3):
 def get_most_commented_posts(count=3):
     commented = Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
     return {'commented': commented}
+
+
+@register.filter(name="markdown")
+def markdown_formatted(text):
+    return mark_safe(markdown.markdown(text))
 
